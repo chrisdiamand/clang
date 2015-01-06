6644aa5ece1d77e4223ff366bb3f4db25347a05f
Chris Diamand <chris@diamand.org>
2015-01-06 09:31:08 +0000

    Start writing code. Doesn't build yet.

--- lib/CodeGen/CGExprScalar.cpp
+++ lib/CodeGen/CGExprScalar.cpp
@@ -470,6 +470,9 @@ public:
       return Builder.CreateFMul(Ops.LHS, Ops.RHS, "mul");
     return Builder.CreateMul(Ops.LHS, Ops.RHS, "mul");
   }
+
+  void EmitCastCheck(Value *src, llvm::Type *DestTy);
+
   /// Create a binary op that checks for overflow.
   /// Currently only supports +, - and *.
   Value *EmitOverflowCheckedBinOp(const BinOpInfo &Ops);
@@ -1315,6 +1318,16 @@ static bool ShouldNullCheckClassCastValue(const CastExpr *CE) {
   return true;
 }
 
+void ScalarExprEmitter::EmitCastCheck(Value *Src, llvm::Type DstTy) {
+  llvm::Function *CheckF = TheModule->getFunction("__is_aU");
+  printf("Has crunch enabled\n");
+  if (!CheckF)
+    return ErrorV("Unknown function: __is_aU");
+  std::vector<Value *> ArgsV;
+  ArgsV.push_back(Src);
+  Builder.CreateCall(CheckF, ArgsV, "crunchcheck");
+}
+
 // VisitCastExpr - Emit code for an explicit or implicit cast.  Implicit casts
 // have to handle a more broad range of conversions than explicit casts, as they
 // handle things like function to ptr-to-function decay etc.
@@ -1355,6 +1368,10 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
       llvm_unreachable("wrong cast for pointers in different address spaces"
                        "(must be an address space cast)!");
     }
+
+    if (CGF.SanOpts.has(SanitizerKind::Crunch))
+      EmitCastCheck(Src, DstTy);
+
     return Builder.CreateBitCast(Src, DstTy);
   }
   case CK_AddressSpaceConversion: {
