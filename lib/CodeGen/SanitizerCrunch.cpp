d4bbfc03965df98d33b2ab1b2c644d2d7eee62bc
Chris Diamand <chris@diamand.org>
2015-02-17 03:19:06 +0000

    Start to emit type name based on LLVM type.

--- lib/CodeGen/SanitizerCrunch.cpp
+++ lib/CodeGen/SanitizerCrunch.cpp
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 #include "CodeGenFunction.h"
 #include "CGCXXABI.h"
 #include "CGDebugInfo.h"
@@ -16,7 +18,10 @@
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/Intrinsics.h"
 #include "llvm/IR/Module.h"
-#include <cstdarg>
+#include <cstring>
+#include <iostream>
+
+#include "SanitizerCrunch.h"
 
 using namespace clang;
 using namespace CodeGen;
@@ -24,23 +29,74 @@ using llvm::Value;
 
 namespace Crunch {
 
+static std::string getUniqtypeName(llvm::Type *Ty) {
+  // Based on TypePrinting::print()
+  switch (Ty->getTypeID()) {
+  case llvm::Type::VoidTyID:    return "void";
+  case llvm::Type::HalfTyID:    return "half";
+  case llvm::Type::FloatTyID:   return "float";
+  case llvm::Type::DoubleTyID:  return "double";
+
+  case llvm::Type::IntegerTyID: {
+    int width = (static_cast<llvm::IntegerType *>(Ty))->getBitWidth();
+    /* Assume these integer types for now (since LLVM doesn't know about
+     * different C int types. */
+    switch (width) {
+    case 8:   return "char";
+    case 16:  return "short";
+    case 32:  return "int";
+    case 64:  return "long";
+    }
+  }
+
+  case llvm::Type::PointerTyID:
+    return "__PTR_" + getUniqtypeName(Ty->getPointerElementType());
+
+  case llvm::Type::StructTyID:
+    /* getStructName returns a value of the form "struct.foo", so remove the
+     * first 7 characters ('struct.') */
+    return Ty->getStructName().substr(7);
+
+  case llvm::Type::FunctionTyID: {
+    llvm::FunctionType *FTy = static_cast<llvm::FunctionType *>(Ty);
+    std::string ret = "__FUN_FROM_";
+
+    int param_num;
+    for (auto it = FTy->param_begin();
+         it != FTy->param_end(); ++FTy, ++param_num) {
+      ret += "_ARG" + std::to_string(param_num) + "_";
+      ret += getUniqtypeName(*it);
+    }
+    ret += "__FUN_TO_" + getUniqtypeName(FTy->getReturnType());
+  }
+
+  default:
+    std::cerr << "unknown: ";
+    Ty->dump();
+  }
+
+  return "<?>";
+}
+
 // GetUniqtype - return the correct uniqtype variable for a given type to
 // check.
-static llvm::Value *GetUniqtype(CodeGenFunction &CGF, CGBuilderTy &Builder,
-                                llvm::LLVMContext &VMContext, llvm::Type *Ty)
-{
+llvm::Value *Check::GetUniqtype(llvm::Type *Ty) {
+  assert(DstTy->isPointerTy() && "Can't check non-pointer destination types");
+
+  llvm::Type *ptrTy = DstTy->getPointerElementType();
+
+  std::cerr << "Converting type: ";
+  ptrTy->dump();
+
+  std::cout << getUniqtypeName(ptrTy) << std::endl;
+
   llvm::Module &TheModule = CGF.CGM.getModule();
   llvm::Type *utTy = llvm::Type::getInt8PtrTy(VMContext);
   llvm::Constant *ret = TheModule.getOrInsertGlobal("__uniqtype_long", utTy);
   return Builder.CreateBitCast(ret, utTy);
 }
 
-// EmitCastCheck - Emit a call to libcrunch to check if the pointer really
-// points to the the type we're casting it to.
-void EmitCastCheck(CodeGenFunction &CGF, CGBuilderTy &Builder,
-                   llvm::LLVMContext &VMContext,
-                   Value *Src, llvm::Type *DstTy)
-{
+void Check::Emit() {
   if (!CGF.SanOpts.has(SanitizerKind::Crunch))
     return;
 
@@ -63,9 +119,17 @@ void EmitCastCheck(CodeGenFunction &CGF, CGBuilderTy &Builder,
   std::vector<Value *> ArgsV;
   ArgsV.push_back(Src);
 
-  ArgsV.push_back(GetUniqtype(CGF, Builder, VMContext, DstTy));
+  ArgsV.push_back(GetUniqtype(DstTy));
 
   Builder.CreateCall(CheckF, ArgsV, "crunchcheck");
+}
+
+void EmitCastCheck(CodeGenFunction &CGF, CGBuilderTy &Builder,
+                   llvm::LLVMContext &VMContext,
+                   Value *Src, llvm::Type *DstTy)
+{
+  Check c(CGF, Builder, VMContext, Src, DstTy);
+  c.Emit();
 }
 
 } // namespace Crunch
