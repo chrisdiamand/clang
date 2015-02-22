75b94b9e89f74afd6241642e67fcb7474f491705
Chris Diamand <chris@diamand.org>
2015-02-22 16:18:20 +0000

    Remove some #include statements.

--- lib/CodeGen/SanitizerCrunch.h
+++ lib/CodeGen/SanitizerCrunch.h
@@ -2,23 +2,7 @@
 #define LLVM_CLANG_LIB_CODEGEN_SANITIZER_CRUNCH_H
 
 #include "CodeGenFunction.h"
-#include "CGCXXABI.h"
-#include "CGDebugInfo.h"
-#include "CGObjCRuntime.h"
 #include "CodeGenModule.h"
-#include "clang/AST/ASTContext.h"
-#include "clang/AST/DeclObjC.h"
-#include "clang/AST/RecordLayout.h"
-#include "clang/AST/StmtVisitor.h"
-#include "clang/Basic/TargetInfo.h"
-#include "clang/Frontend/CodeGenOptions.h"
-#include "llvm/IR/CFG.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/GlobalVariable.h"
-#include "llvm/IR/Intrinsics.h"
-#include "llvm/IR/Module.h"
 #include <cstring>
 
 namespace Crunch {
