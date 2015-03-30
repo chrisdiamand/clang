371c3aba41279e233e8fb389b2103a59c592fdef
Chris Diamand <chris@diamand.org>
2015-03-30 19:59:27 +0100

    Remove debugging prints from offsetof code.

--- lib/CodeGen/Crunch/AllocSite.cpp
+++ lib/CodeGen/Crunch/AllocSite.cpp
@@ -44,13 +44,6 @@ public:
   }
 
   virtual bool VisitOffsetOfExpr(clang::OffsetOfExpr *E) {
-    E->dump();
-    printf("OffsetOfExpr: nexpr = %u, ncomp = %u\n",
-           E->getNumExpressions(), E->getNumComponents());
-    for (unsigned i = 0; i < E->getNumComponents(); ++i) {
-//      const clang::OffsetOfNode &Comp = E->getComponent(i);
- //     Comp->dump();
-    }
     QType = E->getTypeSourceInfo()->getType();
     Found = true;
     return false;
